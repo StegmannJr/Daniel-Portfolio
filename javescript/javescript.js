9c2181df5892faf14698f3d4fc5a862b58428e93
Daniel Stegmann Henriksen <dahe6@elev.tec.dk>
2026-06-12 14:07:53 +0200

    Added Light Switch - Needs to find reason for not working in experiance and Contact

--- javescript/javescript.js
+++ javescript/javescript.js
@@ -15,3 +15,54 @@ function ageCalculator(birth, id) {
     const element = document.getElementById(id);
     element.innerText = age;
 }
+
+(function () {
+    let lightSwitch = document.getElementById("lightSwitch");
+    if (!lightSwitch) {
+        return;
+    }
+
+    function darkMode() {
+        document.body.classList.remove("bg-light", "text-dark");
+        document.body.classList.add("bg-dark", "text-light");
+
+        lightSwitch.checked = true;
+        localStorage.setItem("lightSwitch", "dark");
+    }
+
+    function lightMode() {
+        document.body.classList.remove("bg-dark", "text-light");
+        document.body.classList.add("bg-light", "text-dark");
+
+        lightSwitch.checked = false;
+        localStorage.setItem("lightSwitch", "light");
+    }
+
+    function onToggleMode() {
+        lightSwitch.checked ? darkMode() : lightMode();
+    }
+
+    function getSystemDefaultTheme() {
+        const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
+        if (darkThemeMq.matches) {
+            return "dark";
+        }
+        return "light";
+    }
+
+    function setup() {
+        var settings = localStorage.getItem("lightSwitch");
+        if (settings == null) {
+            settings = getSystemDefaultTheme();
+        }
+
+        if (settings = "dark") {
+            lightSwitch.checked = true;
+        }
+
+        lightSwitch.addEventListener("change", onToggleMode);
+        onToggleMode();
+    }
+
+    setup();
+})();
